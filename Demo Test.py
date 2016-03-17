e723a47adb5af211ef851c06c3ebb828693ac27b
zjb19911005 <397563682@qq.com>
2016-03-17 15:46:27 +0800

    demo

--- Demo Test.py
+++ Demo Test.py
@@ -1,5 +1,23 @@
 #coding=utf-8
+from django.conf.locale import te
+
 _author__ = 'Junior'
 
 #用python来做接口测试
 import requests
+
+def shishike(url):
+    headers={}
+    params={}
+    req=requests.post(url,headers,params=params)
+    #设置编码
+    req.encoding='utf-8'
+    print(req,text)
+
+
+
+
+if __name__=='__main__':
+    url='http://www.baidu.com'
+    shishike(url)
+
