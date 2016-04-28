ce1366ca14b14376ddfac3aa623ce1e7f5bdb12e
zjb19911005 <397563682@qq.com>
2016-04-28 18:14:20 +0800

    添加SQL文件

--- Open SQL.py
+++ Open SQL.py
@@ -10,12 +10,13 @@ connect=MySQLdb.connect(
 )
 cur=connect.cursor()
 #获取数据库有多少条数据
-a=cur.execute("SELECT * FROM trade WHERE shop_identy = 810002790 AND source = 3 AND trade_status = 3 AND trade_time>'2016-04-28' ")
-print a
+a=cur.execute("SELECT * FROM trade WHERE shop_identy = 810002790 AND source = 3 AND trade_status = 3 AND trade_time>'2016-04-28' limit 10 ")
+print '本次数据查询出合适的数据为%d条' %a
+print '详情如下'
 #打印列表中的数据
 info=cur.fetchmany(a)
-for ii in  info:
-    print ii
+for i in  info:
+    print i
 #关闭表单
 cur.close()
 connect.commit()
